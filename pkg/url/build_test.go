804e5b227c61918e473fe0935ccf07a5bb805ae9
Brandon Whitfield <whitfield.b89@gmail.com>
2025-08-21 14:00:36 -0500

    fixes test

--- pkg/url/build_test.go
+++ pkg/url/build_test.go
@@ -10,7 +10,7 @@ func Test_Build_Base(t *testing.T) {
 	actual, e := Build("https://jerry.wilson")
 
 	assert.Nil(t, e)
-	assert.Equal(t, "https://jerry.wilson", actual)
+	assert.Equal(t, "https://jerry.wilson", *actual)
 }
 func Test_Build_Hostname_Error(t *testing.T) {
 	_, e := Build("*****")
@@ -21,17 +21,17 @@ func Test_Build_Base_Trailing_Slash(t *testing.T) {
 	actual, e := Build("https://jerry.wilson/")
 
 	assert.Nil(t, e)
-	assert.Equal(t, "https://jerry.wilson/", actual)
+	assert.Equal(t, "https://jerry.wilson/", *actual)
 }
 func Test_Build_Path(t *testing.T) {
 	actual, e := Build("https://jerry.wilson", "a", "b")
 
 	assert.Nil(t, e)
-	assert.Equal(t, "https://jerry.wilson/a/b", actual)
+	assert.Equal(t, "https://jerry.wilson/a/b", *actual)
 }
 func Test_Build_Path_Trailing_Slash(t *testing.T) {
 	actual, e := Build("https://jerry.wilson/", "a", "b")
 
 	assert.Nil(t, e)
-	assert.Equal(t, "https://jerry.wilson/a/b", actual)
+	assert.Equal(t, "https://jerry.wilson/a/b", *actual)
 }
