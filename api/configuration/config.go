619f677121a6cbeeab105c16afc5631e58df401f
Narsimham Chelluri (Narsa) <narsa@kusari.dev>
2025-09-17 21:43:56 -0300

    Update config for Inspector status check
    Signed-off-by: Narsimham Chelluri (Narsa) <narsa@kusari.dev>

--- api/configuration/config.go
+++ api/configuration/config.go
@@ -1,6 +1,8 @@
 package configuration
 
 type Config struct {
-	GitHubActionVersionPinningCheckEnabled bool `yaml:"github_action_version_pinning_check_enabled"`
-	ContainerVersionPinningCheckEnabled    bool `yaml:"container_version_pinning_check_enabled"`
+	GitHubActionVersionPinningCheckEnabled bool   `yaml:"github_action_version_pinning_check_enabled"`
+	ContainerVersionPinningCheckEnabled    bool   `yaml:"container_version_pinning_check_enabled"`
+	StatusCheckName                        string `yaml:"status_check_name"`       // Name of the status check (default: "Kusari Inspector")
+	PostCommentOnFailure                   bool   `yaml:"post_comment_on_failure"` // Also post comment when status check fails
 }
