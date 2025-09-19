6e16b243eb04cb3b4d90d0dacb2c4cb87d2037ba
Ben Cotton <ben@kusari.dev>
2025-09-19 13:58:52 -0400

    Add an update-config subcommand
    Signed-off-by: Ben Cotton <ben@kusari.dev>

---
+++ kusari/cmd/configuration_update_config.go
@@ -0,0 +1,28 @@
+// Copyright Kusari, Inc. and contributors <https://www.kusari.dev/>
+// SPDX-License-Identifier: MIT
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/kusaridev/kusari-cli/pkg/configuration"
+	"github.com/spf13/cobra"
+)
+
+func updateConfig() *cobra.Command {
+	updatecmd.RunE = func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
+
+		return configuration.UpdateConfig()
+	}
+
+	return updatecmd
+}
+
+var updatecmd = &cobra.Command{
+	Use:   "update-config",
+	Short: fmt.Sprintf("Update %s config file", configuration.ConfigFilename),
+	Long: fmt.Sprintf("Update a %s config file for kusari-cli "+
+		"with new values.", configuration.ConfigFilename),
+}
--- pkg/configuration/config.go
+++ pkg/configuration/config.go
@@ -1,8 +1,10 @@
 package configuration
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/kusaridev/kusari-cli/api/configuration"
 	"gopkg.in/yaml.v3"
@@ -12,15 +14,15 @@ const ConfigFilename = "kusari.yaml"
 
 var ErrFileExists = fmt.Errorf("file %s exists, not overwriting (specify '--force' to overwrite)", ConfigFilename)
 
-func GenerateConfig(forceWrite bool) error {
-	DefaultConfig := configuration.Config{
-		GitHubActionVersionPinningCheckEnabled: true,
-		ContainerVersionPinningCheckEnabled:    true,
-		StatusCheckName:                        "Kusari Inspector",
-		PostCommentOnFailure:                   true,
-		PostCommentOnSuccess:                   false,
-	}
+var DefaultConfig = configuration.Config{
+	GitHubActionVersionPinningCheckEnabled: true,
+	ContainerVersionPinningCheckEnabled:    true,
+	StatusCheckName:                        "Kusari Inspector",
+	PostCommentOnFailure:                   true,
+	PostCommentOnSuccess:                   false,
+}
 
+func GenerateConfig(forceWrite bool) error {
 	// check to see if the config file already exists
 	_, err := os.Stat(ConfigFilename)
 	if (err == nil) && !forceWrite {
@@ -33,4 +35,85 @@ func GenerateConfig(forceWrite bool) error {
 	}
 
 	return os.WriteFile(ConfigFilename, []byte(cfgYaml), 0600)
+}
+
+func UpdateConfig() error {
+	// Check to see if the config file already exists. If it does not,
+	// just run the Generate instead.
+	_, err := os.Stat(ConfigFilename)
+	if errors.Is(err, os.ErrNotExist) {
+		return GenerateConfig(false)
+	}
+
+	// Read the config file to get existing values
+	configData, err := os.ReadFile(ConfigFilename)
+	if err != nil {
+		return fmt.Errorf("failed to read file %s: %w", ConfigFilename, err)
+	}
+
+	var existingConfig map[string]interface{}
+	if err := yaml.Unmarshal(configData, &existingConfig); err != nil {
+		return fmt.Errorf("failed to parse config file: %w", err)
+	}
+
+	updatedConfig := mergeConfigs(DefaultConfig, existingConfig)
+
+	cfgYaml, err := yaml.Marshal(updatedConfig)
+	if err != nil {
+		return fmt.Errorf("error marshaling default config yaml: %w", err)
+	}
+
+	return os.WriteFile(ConfigFilename, []byte(cfgYaml), 0600)
+}
+
+// A function to compare the configs and merge them together
+func mergeConfigs(defaultConfig configuration.Config, existingConfig map[string]interface{}) configuration.Config {
+	result := defaultConfig
+
+	// Use reflection to iterate over all struct fields
+	resultValue := reflect.ValueOf(&result).Elem()
+	resultType := reflect.TypeOf(result)
+
+	for i := 0; i < resultType.NumField(); i++ {
+		field := resultType.Field(i)
+
+		// Get the JSON tag name for this field
+		yamlTag := field.Tag.Get("yaml")
+
+		// Check if this field was present in the original YAML
+		if val, exists := existingConfig[yamlTag]; exists {
+			resultFieldValue := resultValue.Field(i)
+
+			// Only set if the field is settable
+			if resultFieldValue.CanSet() {
+				// Convert and set the value based on field type
+				switch resultFieldValue.Kind() {
+				case reflect.Bool:
+					if boolVal, ok := val.(bool); ok {
+						resultFieldValue.SetBool(boolVal)
+					}
+				case reflect.String:
+					if stringVal, ok := val.(string); ok {
+						resultFieldValue.SetString(stringVal)
+					}
+				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+					// YAML can parse numbers as int, int64, or sometimes float64
+					switch v := val.(type) {
+					case int:
+						resultFieldValue.SetInt(int64(v))
+					case int64:
+						resultFieldValue.SetInt(v)
+					case float64:
+						resultFieldValue.SetInt(int64(v))
+					}
+				case reflect.Float32, reflect.Float64:
+					if floatVal, ok := val.(float64); ok {
+						resultFieldValue.SetFloat(floatVal)
+					}
+				}
+			}
+		}
+	}
+
+	return result
 }
