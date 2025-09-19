0a95978469f9ec89cc42c2080887f7f466404b72
Narsimham Chelluri (Narsa) <narsa@kusari.dev>
2025-09-19 10:37:13 -0300

    Add struct field comments per code-review
    Signed-off-by: Narsimham Chelluri (Narsa) <narsa@kusari.dev>

--- api/configuration/config.go
+++ api/configuration/config.go
@@ -1,8 +1,8 @@
 package configuration
 
 type Config struct {
-	GitHubActionVersionPinningCheckEnabled bool   `yaml:"github_action_version_pinning_check_enabled"`
-	ContainerVersionPinningCheckEnabled    bool   `yaml:"container_version_pinning_check_enabled"`
-	StatusCheckName                        string `yaml:"status_check_name"`       // Name of the status check (default: "Kusari Inspector")
-	PostCommentOnFailure                   bool   `yaml:"post_comment_on_failure"` // Also post comment when status check fails
+	GitHubActionVersionPinningCheckEnabled bool   `yaml:"github_action_version_pinning_check_enabled"` // Check whether GH Action versions are pinned
+	ContainerVersionPinningCheckEnabled    bool   `yaml:"container_version_pinning_check_enabled"`     // Check whether container versions are pinned
+	StatusCheckName                        string `yaml:"status_check_name"`                           // Name of the status check (default: "Kusari Inspector")
+	PostCommentOnFailure                   bool   `yaml:"post_comment_on_failure"`                     // Also post comment when status check fails
 }
